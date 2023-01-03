b72aabd9dedaae0f07e876b6b02b7a490c110e33
Johnny Blasius Seubert <68622224+johnnyseubert@users.noreply.github.com>
2023-01-03 16:24:13 -0300

    creating entities with relations

---
+++ src/database/entities/Category.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { Tables } from '../Tables';
+import { v4 as uuid } from 'uuid';
+
+@Entity(Tables.Categories)
+export class Category {
+   constructor() {
+      if (!this.id) {
+         this.id = uuid();
+      }
+   }
+
+   @PrimaryColumn()
+   id: string;
+
+   @Column()
+   name: string;
+
+   @Column()
+   description: string;
+
+   @CreateDateColumn()
+   created_at: Date;
+}
